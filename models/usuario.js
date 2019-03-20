79f1a156b8f8c87c4a13799168e3d1676af2e622
Sebastian <sebanicosp@gmail.com>
2019-03-20 14:08:00 -0300

    Add: User Model

---
+++ models/usuario.js
@@ -0,0 +1,33 @@
+mongoose = require('mongoose');
+
+Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, 'El email es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+
+});
+
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser unico' });
+
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
